8f0a8aba5ea0363538fe26bb44d942a2c69d831a
Karol Kuc <karolkuc098@gmail.com>
2024-03-01 22:36:43 +0000

    Initial commit

---
+++ src/main/java/com/happcode/frugalforge/FrugalForgeApplication.java
@@ -0,0 +1,13 @@
+package com.happcode.frugalforge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrugalForgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrugalForgeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/happcode/frugalforge/FrugalForgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happcode.frugalforge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrugalForgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
